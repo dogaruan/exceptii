b7de149f195acf6996662a5aaf368e5e0d37cd8f
dogaruan <dogaru.andrei08@yahoo.com>
2024-05-27 19:41:00 +0300

    Am facut si problema 2

--- exceptii.py
+++ exceptii.py
@@ -1,14 +1,26 @@
 # 2.Fiind dat un dictionar {"a":1,"b":2,"c":3} scrieti o functie care primeste ca parametru cheia si returneaza valoarea
 
-def cheia_valoare(dictionar, cheie):
+# def cheia_valoare(dictionar, cheie):
+#     try:
+#         valoare = dictionar[cheie]
+#         return valoare
+#     except KeyError:
+#         return f"Cheia '{cheie}' nu există în dicționar."
+
+# dictionar = {"a": 1, "b": 2, "c": 3}
+
+# cheie = input("Introduceți cheia: ")
+# valoare = cheia_valoare(dictionar, cheie)
+# print(f"Valoarea pentru cheia '{cheie}' este: {valoare}")
+
+# 1. Scrieti o functie care citeste un string de la tastatura si afiseaza lungimea lui. Tratati cazul in care textul nu este string
+def len_txt():
     try:
-        valoare = dictionar[cheie]
-        return valoare
-    except KeyError:
-        return f"Cheia '{cheie}' nu există în dicționar."
+        text = input("Introduceți un text: ")
+        
+        print(f"Lungimea textului introdus este: {len(text)}")
+    
+    except ValueError as e:
+        print(e)
 
-dictionar = {"a": 1, "b": 2, "c": 3}
-
-cheie = input("Introduceți cheia: ")
-valoare = cheia_valoare(dictionar, cheie)
-print(f"Valoarea pentru cheia '{cheie}' este: {valoare}")
+len_txt()
